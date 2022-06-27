11908807b4819b697a415a3357037799f506297c
Alberta Lynn Brubaker <findingalberta@gmail.com>
2022-06-27 11:51:54 -0500

    commit tree edf241b84e59cd9d2bb3520237f68b71d2135501

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-
 // Write your code here
+
+let num1;
+let num2;
+
+
+let multiply = (num1 * num2) === 62;
+
+let random  = parseInt(Math.floor(Math.random() * 10) +1);
+
+let num3;
+let num4;
+
+let mod = (num3 % num4 ) === 4;
+
+let max = Math.max(1, 2, 5 , 20);
